41b4074709e7b99dc47fe678575bb1fed693eb42
igor_batyukov <igor.s.batyukov@gmail.com>
2023-01-30 18:01:37 +0300

    Initial decomposition of operations.app model

--- operations/models.py
+++ operations/models.py
@@ -3,7 +3,7 @@ from fleet.models import Vessel
 from geo.models import SeaPort
 
 
-class Agency(models):
+class Agency(models.Model):
     name = models.CharField(max_length=100)
     location = models.ForeignKey(SeaPort, on_delete=models.CASCADE)
     office_phone_num = models.CharField(max_length=25)
@@ -19,15 +19,44 @@ class Agency(models):
 
 
 class Voyage(models.Model):
+    BALLAST = 'ballast'
+    LOADED = 'loaded'
+
+    VOYAGE_TYPE = [
+        (BALLAST, 'Ballast leg'),
+        (LOADED, 'Laden leg'),
+    ]
+
     voy_num = models.CharField(max_length=15)
-    departure = models.ForeignKey(SeaPort, on_delete=models.CASCADE)
-    destination = models.ManyToManyField(SeaPort)
+    vessel = models.ForeignKey(Vessel, on_delete=models.CASCADE)
+    type = models.CharField(max_length=10, choices=VOYAGE_TYPE, default=BALLAST)
+    departure = models.ForeignKey(SeaPort, on_delete=models.CASCADE, related_name='departure_port')
+    destination = models.ManyToManyField(SeaPort, related_name='destination_port')
+    eta = models.DateTimeField()
     is_completed = models.BooleanField(default=False)
     agency = models.ManyToManyField(Agency)
 
 
 class Cargo(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    CRUDE_OIL = 'crude_oil'
+    FUEL_OIL = 'fuel_oil'
+    DIESEL_OIL = 'diesel_oil'
+    GRAIN_BULK = 'grain_bulk'
+    COAL_BULK = 'coal_bulk'
+    GENERAL_CARGO = 'general_cargo'
+
+    CARGO_TYPES = [
+        (CRUDE_OIL, 'Crude Oil'),
+        (FUEL_OIL, 'Fuel Oil'),
+        (DIESEL_OIL, 'Diesel Oil'),
+        (GRAIN_BULK, 'Grain in Bulk'),
+        (COAL_BULK, 'Coal in Bulk'),
+        (GENERAL_CARGO, 'General Cargo'),
+    ]
+
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=20, choices=CARGO_TYPES, default=CRUDE_OIL)
+    vessel = models.ManyToManyField(Vessel)
 
     def __str__(self):
         return self.name
@@ -36,23 +65,86 @@ class Cargo(models.Model):
         ordering = ['name']
 
 
-class NoonReport(models.Model):
+class CargoOps(models.Model):
+    LOADING = 'loading'
+    DISCHARGING = 'discharging'
 
-    OPERATION = [
-        ('sea', 'Vessel is at sea'),
-        ('port', 'Vessel is in port'),
-        ('drifting', 'Vessel is drifting'),
-        ('anchorage', 'Vessel is at anchorage'),
+    CARGO_OPS_TYPES = [
+        (LOADING, 'Loading'),
+        (DISCHARGING, 'Discharging'),
     ]
 
+    ops_date_time = models.DateTimeField()
+    utc_offset = models.FloatField()
+    voyage = models.ForeignKey(Voyage, on_delete=models.CASCADE)
     vessel = models.ForeignKey(Vessel, on_delete=models.CASCADE)
-    voy_num = models.CharField(max_length=15)
+    cargo = models.OneToOneField(Cargo, on_delete=models.CASCADE)
+    type = models.CharField(max_length=20, choices=CARGO_OPS_TYPES, default=LOADING)
+    quantity = models.FloatField()
+    bl_number = models.CharField(max_length=30)
+    shipper = models.CharField(max_length=70)
+    consignee = models.CharField(max_length=70)
+
+    def __str__(self):
+        return f'{self.vessel} - {self.type}: {self.ops_date_time}'
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['vessel', 'cargo', 'ops_date_time'], name='unique_cargo_ops')
+        ]
+
+
+class Bunker(models.Model):
+    FUEL_OIL = 'FO'
+    DIESEL_OIL = 'DO'
+    MARINE_GAS_OIL = 'MGO'
+    VERY_LOW_SULPHUR_FUEL_OIL = 'VLSFO'
+
+    BUNKER_TYPES = [
+        (FUEL_OIL, 'Fuel Oil'),
+        (DIESEL_OIL, 'Diesel Oil'),
+        (MARINE_GAS_OIL, 'Marine Gas Oil'),
+        (VERY_LOW_SULPHUR_FUEL_OIL, 'Very Low Sulphur Fuel Oil'),
+    ]
+
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=20, choices=BUNKER_TYPES, default=DIESEL_OIL)
+    vessel = models.ManyToManyField(Vessel)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class BunkerOps(models.Model):
+    ops_date_time = models.DateTimeField()
+    utc_offset = models.FloatField()
+    voyage = models.ForeignKey(Voyage, on_delete=models.CASCADE)
+    location = models.ForeignKey(SeaPort, on_delete=models.CASCADE)
+    bunker = models.ForeignKey(Bunker, on_delete=models.CASCADE)
+    supplier = models.CharField(max_length=100)
+
+
+class VesselPosition(models.Model):
+    AT_SEA = 'sea'
+    IN_PORT = 'port'
+    DRIFTING = 'drifting'
+    ANCHORAGE = 'anchorage'
+
+    VESSEL_OPERATION = [
+        (AT_SEA, 'Vessel is at sea'),
+        (IN_PORT, 'Vessel is in port'),
+        (DRIFTING, 'Vessel is drifting'),
+        (ANCHORAGE, 'Vessel is at anchorage'),
+    ]
+
+    voy_num = models.ForeignKey(Voyage, on_delete=models.CASCADE)
     report_date = models.DateField()
     utc_offset = models.FloatField()
     current_position = models.CharField(max_length=25)
-    operation = models.CharField(max_length=15, choices=OPERATION, default='sea')
-    destination = models.ForeignKey(SeaPort, on_delete=models.CASCADE)
-    eta = models.DateTimeField()
+    operation = models.CharField(max_length=15, choices=VESSEL_OPERATION, default=AT_SEA)
     distance_daily = models.PositiveIntegerField()
     speed_avg = models.PositiveIntegerField()
     course_avg = models.PositiveIntegerField()
@@ -61,75 +153,4 @@ class NoonReport(models.Model):
     swell_dir = models.PositiveIntegerField()
     wind_bft = models.PositiveIntegerField()
     wind_dir = models.PositiveIntegerField()
-    bunker_vlsfo = models.FloatField()
-    bunker_do = models.FloatField()
-
-    def __str__(self):
-        return f'Noon position: {self.vessel} - {self.voy_num} - {self.report_date}'
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['voy_num', 'report_date'], name='unique_noon_report')
-        ]
-
-
-class DepartureReport(models.Model):
-
-    CONDITION = [
-        ('ballast', 'Vessel is in ballast condition'),
-        ('loaded', 'Vessel is in loaded condition'),
-    ]
-
-    vessel = models.ForeignKey(Vessel, on_delete=models.CASCADE)
-    voy_num = models.CharField(max_length=15)
-    report_date_time = models.DateTimeField()
-    utc_offset = models.FloatField()
-    destination = models.ForeignKey(SeaPort, on_delete=models.CASCADE)
-    bunker_vlsfo = models.FloatField()
-    bunker_do = models.FloatField()
-    condition = models.CharField(max_length=10, choices=CONDITION, default='ballast')
-    agency = models.CharField(max_length=100, null=True, blank=True)
-
-    def __str__(self):
-        return f'Departure: {self.vessel} - {self.voy_num} - {self.report_date_time}'
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['voy_num', 'report_date_time'], name='unique_departure_report')
-        ]
-
-
-class ArrivalReport(models.Model):
-    vessel = models.ForeignKey(Vessel, on_delete=models.CASCADE)
-    voy_num = models.CharField(max_length=15)
-    report_date_time = models.DateTimeField()
-    utc_offset = models.FloatField()
-    arrival_port = models.ForeignKey(SeaPort, on_delete=models.CASCADE)
-    distance_daily = models.PositiveIntegerField()
-    bunker_vlsfo = models.FloatField()
-    bunker_do = models.FloatField()
-
-    def __str__(self):
-        return f'Arrival: {self.vessel} - {self.voy_num} - {self.report_date_time}'
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['voy_num', 'report_date_time'], name='unique_arrival_report')
-        ]
-
-
-class CargoOnBoard(models.Model):
-    vessel = models.ForeignKey(Vessel, on_delete=models.CASCADE)
-    voy_num = models.CharField(max_length=15)
-    report_date_time = models.DateTimeField()
-    cargo = models.ManyToManyField(Cargo)
-
-
-class BunkeringReport(models.Model):
-    bunkering_date_time = models.DateTimeField()
-    location = models.ForeignKey(SeaPort, on_delete=models.CASCADE)
-    vlsfo_qty = models.FloatField()
-    do_qty = models.FloatField()
-    supplier = models.CharField(max_length=100)
-
 
