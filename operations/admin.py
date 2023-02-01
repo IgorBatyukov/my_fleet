cf738532dcf0585f1e9a43929359c9ea95ec788e
igor_batyukov <igor.s.batyukov@gmail.com>
2023-02-01 14:34:49 +0300

    Initial operations.app admin registration

--- operations/admin.py
+++ operations/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Agency)
+class AgencyAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Voyage)
+class VoyageAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Cargo)
+class CargoAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.CargoOps)
+class CargoOpsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Bunker)
+class BunkerAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.BunkerOps)
+class BunkerOpsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.BunkerDailyBalance)
+class BunkerDailyBalanceAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.VesselPosition)
+class VesselPositionAdmin(admin.ModelAdmin):
+    pass
+
+
+
