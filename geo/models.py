e983c2c577620a8e7ee7a2d2830549eccd396736
igor_batyukov <igor.s.batyukov@gmail.com>
2023-02-01 19:33:26 +0300

    Added methods for City model. Added Shipyard model

--- geo/models.py
+++ geo/models.py
@@ -32,13 +32,23 @@ class Airport(models.Model):
     city = models.ForeignKey(City, on_delete=models.CASCADE)
     iata_code = models.CharField(max_length=3)
 
+    def get_city(self):
+        return self.city
+
 
 class SeaPort(models.Model):
     name = models.CharField(max_length=50)
     country = models.ForeignKey(Country, on_delete=models.CASCADE)
+    ref_code = models.CharField(max_length=10, null=True)
 
     def __str__(self):
         return f'{self.name}, {self.country}'
+
+    class Meta:
+        ordering = ['name']
+
+    def get_country(self):
+        return self.country
 
 
 class EducationCenter(models.Model):
@@ -61,3 +71,19 @@ class MedicalCenter(models.Model):
 
     def get_location(self):
         return self.city
+
+
+class Shipyard(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.ForeignKey(City, on_delete=models.CASCADE)
+    country = models.ForeignKey(Country, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def get_city(self):
+        return self.city
+
+    def get_country(self):
+        return self.country
+
