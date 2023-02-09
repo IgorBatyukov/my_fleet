3b45ae3ace647119e3d86fbcbf8452c116b664bb
igor_batyukov <igor.s.batyukov@gmail.com>
2023-02-09 20:32:02 +0300

    Initial view models

--- crew/views.py
+++ crew/views.py
@@ -1,20 +1,34 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from .models import CrewMember
+from django.views.generic import ListView, DetailView, TemplateView
+from .models import CrewMember, VesselsSchedule
+from fleet.models import Vessel
 
 
-class CrewingMainView(ListView):
-    template_name = 'crew/crew_list.html'
-    model = CrewMember
-    context_object_name = 'crew'
+class CrewingMainView(TemplateView):
+    template_name = 'crew/home.html'
 
 
 class VesselsMainView(ListView):
-    pass
+    template_name = 'crew/vessels.html'
+    model = VesselsSchedule
+    context_object_name = 'vessels'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        fleet_vessels = queryset.filter(fleet='A')
+        return fleet_vessels
 
 
-class VesselDetailsView(DetailView):
-    pass
+class VesselDetailsView(ListView):
+    template_name = 'crew/vessel_details.html'
+    model = VesselsSchedule
+    context_object_name = 'vessel'
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        id = self.kwargs['pk']
+        vessel_details = queryset.get(vessel_id=id)
+        return vessel_details
 
 
 class CrewMainView(ListView):
