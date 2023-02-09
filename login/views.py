0b55373f093223aa6514f70f8126cbee82e64d95
igor_batyukov <igor.s.batyukov@gmail.com>
2023-02-09 20:32:39 +0300

    Added methods to Login model

--- login/views.py
+++ login/views.py
@@ -1,7 +1,15 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
 
 
 class LoginUserView(LoginView):
     form_class = AuthenticationForm
     template_name = 'login/login.html'
+
+    def check_user_group(self, group_name):
+        return self.request.user.groups.filter(name=group_name).exists()
+
+    def get_success_url(self):
+        if self.check_user_group('crewing_manager'):
+            return reverse_lazy('crewing_home')
