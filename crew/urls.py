9a49954578f44ca00d69a0c27c1f87a31c7eaa03
igor_batyukov <igor.s.batyukov@gmail.com>
2023-02-24 11:50:57 +0300

    Added paths for a crew change

--- crew/urls.py
+++ crew/urls.py
@@ -1,13 +1,18 @@
-from django.urls import path
-from crew import views
+from django.urls import path, register_converter, re_path
+from crew import views, converters
 
+register_converter(converters.DateConverter, 'date')
 
 urlpatterns = [
 
     path('', views.CrewingMainView.as_view(), name='crewing_home'),
     path('vessels/', views.VesselsMainView.as_view(), name='crewing_vessels'),
     path('vessels/<int:pk>', views.VesselDetailsView.as_view(), name='vessel_details'),
-    path('crew/', views.CrewingMainView.as_view(), name='crew'),
-    path('crew/<int:pk>', views.CrewDetailsView.as_view()),
+    path('crew/', views.CrewListView.as_view(), name='crew'),
+    path('crew/<int:pk>', views.CrewDetailsView.as_view(), name='crew_member_details'),
+    path('crewchange/', views.CrewChangeView.as_view(), name='crew_change'),
+    path('crewchange/<str:vessel__name>/<date:date>/<str:port__name>/<str:type>',
+         views.CrewChangeDetailsView.as_view(),
+         name='crew_change_details'),
 
 ]
