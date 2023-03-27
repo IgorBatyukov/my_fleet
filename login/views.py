696496f38177eaf60bd13ad5ccac546a83eaa0e0
IgorBatyukov <igor.s.batyukov@gmail.com>
2023-03-27 20:53:12 +0300

    Removed user group check

--- login/views.py
+++ login/views.py
@@ -1,5 +1,5 @@
-from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
 from django.urls import reverse_lazy
 
 
@@ -7,9 +7,6 @@ class LoginUserView(LoginView):
     form_class = AuthenticationForm
     template_name = 'login/login.html'
 
-    def check_user_group(self, group_name):
-        return self.request.user.groups.filter(name=group_name).exists()
-
     def get_success_url(self):
-        if self.check_user_group('crewing_manager'):
+        if self.request.user.is_authenticated:
             return reverse_lazy('crewing_home')
