61e45671a81287144bf6dc422374c96021f9a11e
IgorBatyukov <igor.s.batyukov@gmail.com>
2023-03-27 20:49:33 +0300

    Added forms for Contract and CrewChange models

--- crew/forms.py
+++ crew/forms.py
@@ -1,3 +1,24 @@
 from django import forms
-from .models import CrewMember
+from .models import CrewChange, Contract
 
+
+class CrewChangeForm(forms.ModelForm):
+
+    class Meta:
+        model = CrewChange
+        fields = ['crew', 'vessel', 'date', 'port', 'type', 'agency', 'manager']
+
+        widgets = {
+            'date': forms.DateInput(attrs={'type': 'date'}),
+        }
+
+
+class ContractForm(forms.ModelForm):
+
+    class Meta:
+        model = Contract
+        fields = ['manager', 'crew', 'duration', 'offset', 'signed_date', 'finished_date', 'vessel', 'rank']
+
+        widgets = {
+            'signed_date': forms.DateInput(attrs={'type': 'date'}),
+        }
