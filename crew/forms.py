21369dd4021fc758abe6febd9b2198a79e19729d
IgorBatyukov <igor.s.batyukov@gmail.com>
2023-03-28 17:57:59 +0300

    Added create, update, delete forms for crew change, crew, certificates, contract models

--- crew/forms.py
+++ crew/forms.py
@@ -1,9 +1,8 @@
 from django import forms
-from .models import CrewChange, Contract
+from .models import CrewChange, Contract, CrewMember, CrewCertification
 
 
 class CrewChangeForm(forms.ModelForm):
-
     class Meta:
         model = CrewChange
         fields = ['crew', 'vessel', 'date', 'port', 'type', 'agency', 'manager']
@@ -14,11 +13,51 @@ class CrewChangeForm(forms.ModelForm):
 
 
 class ContractForm(forms.ModelForm):
-
     class Meta:
         model = Contract
         fields = ['manager', 'crew', 'duration', 'offset', 'signed_date', 'finished_date', 'vessel', 'rank']
 
         widgets = {
             'signed_date': forms.DateInput(attrs={'type': 'date'}),
+            'finished_date': forms.DateInput(attrs={'type': 'date'}),
+        }
+
+
+class CrewMemberEditForm(forms.ModelForm):
+    class Meta:
+        model = CrewMember
+        fields = [
+            'name',
+            'father_name',
+            'surname',
+            'birth_date',
+            'phone',
+            'email',
+            'location',
+            'marriage_status',
+            'rank'
+        ]
+
+
+class CrewCertificateEditForm(forms.ModelForm):
+
+    class Meta:
+        model = CrewCertification
+        fields = ['cert_number', 'valid_from', 'valid_to']
+
+        widgets = {
+            'valid_from': forms.DateInput(attrs={'type': 'date'}),
+            'valid_to': forms.DateInput(attrs={'type': 'date'}),
+        }
+
+
+class CrewCertificateAddForm(forms.ModelForm):
+
+    class Meta:
+        model = CrewCertification
+        fields = ['cert', 'cert_number', 'valid_from', 'valid_to']
+
+        widgets = {
+            'valid_from': forms.DateInput(attrs={'type': 'date'}),
+            'valid_to': forms.DateInput(attrs={'type': 'date'}),
         }
